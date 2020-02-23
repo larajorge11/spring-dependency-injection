3caa48051e48f8d8181acf9e5468e9825f9c2f3d
jorgelara <Jorge Lara>
2020-02-23 09:32:36 -0500

    Jorge Lara - Initial commit - Dependency Injection examples

---
+++ src/main/java/com/tutorial/di/springframeworkdi/SpringframeworkDiApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.di.springframeworkdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframeworkDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/di/springframeworkdi/SpringframeworkDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.di.springframeworkdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
